b3e541208637ae73d099f56f57de579636ec3e6d
siddharth_dumbre <dumbre.si@northeastern.edu>
2024-05-30 22:55:04 -0400

    Delete button and number check for amount

--- budget_calculator/src/components/FormItem.jsx
+++ budget_calculator/src/components/FormItem.jsx
@@ -2,37 +2,33 @@ import React ,{ useState } from "react";
 import Categories from "../enum/categories";
 
 
-const FormItem =({ item, index, handleItemChange })=>{
+const FormItem =({ item, index, handleItemChange,deleteItem })=>{
     const [itemName, setItemName]=useState('');
     const [amount, setAmount] = useState('');
     const [category, setCategory] = useState(Categories.MISCELLANEOUS);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        addItem({ itemName, amount: parseFloat(amount), category });
-        setItemName('');
-        setAmount('');
-        setCategory(Categories.RENT);
-      };
-
-      const addItem = (item) => {
-        // setExpenditures(prevExpenditures => ({
-        //   ...prevExpenditures,
-        //   [item.category]: [...prevExpenditures[item.category], item]
-        // }));
-      };
-
     return(
-    <div className="flex space-x-4 mb-4">
-        <input type="text" value={itemName} onChange={(e)=> setItemName(e.target.value)} placeholder="Enter Item Name" required />
-        <input type="text" value={amount} onChange={(e)=> setAmount(e.target.value)} placeholder="Enter Amount" required/>
-        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+    <div className="flex justify-between space-x-4 mb-4">
+        <input type="text" value={itemName} onChange={(e)=> setItemName(e.target.value)} placeholder="Enter Item Name" required className="border border-gray-300 p-2 flex-1 rounded"/>
+        <input type="text" value={amount} onChange={(e)=> setAmount(e.target.value)} placeholder="Amount" required className="border border-gray-300 p-2 w-24 rounded"
+        onKeyPress={(event) => {
+          if (!/[0-9]/.test(event.key)) {
+            event.preventDefault();
+          }
+        }}/>
+        <select value={category} onChange={(e) => setCategory(e.target.value)} className="border border-gray-300 p-2 rounded">
         {Object.values(Categories).map((category) => (
           <option key={category} value={category}>
             {category}
           </option>
         ))}
         </select>
+        <button
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => deleteItem(index)}
+      >
+        Delete
+      </button>
     </div>
     );
 }
